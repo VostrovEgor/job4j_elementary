ce5d04897053384bf447699f499a244cb34b5537
vostrov <egor.voctrov.samara@gmail.com>
2024-01-12 00:29:51 +0400

    6. Идеальный вес

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        double result = (height - 100) * 1.15;
+        return result;
+    }
+
+    public static double womanWeight(short height) {
+        double result = (height - 110) * 1.15;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        short height = 178;
+        double man = Fit.manWeight(height);
+        double woman = Fit.womanWeight(height);
+        System.out.println("Man 178 is " + man);
+        System.out.println("Woman 178 is " + woman);
+    }
+
+}
