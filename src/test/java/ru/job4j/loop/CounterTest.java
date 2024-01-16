df5889f328ce7579934795dec999a6116a2ebb87
vostrov <egor.voctrov.samara@gmail.com>
2024-01-16 22:15:13 +0400

    5.1. Подсчет суммы чётных чисел в диапазоне

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -4,13 +4,21 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CounterTest {
+    @Test
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
 
     @Test
-    void sum1and2in3() {
-        int a = 0;
-        int b = 2;
-        int result = Counter.sum(a, b);
-        int expected = 3;
+    public void whenSumEvenNumbersFromTwoToTwelveThenFortyTwo() {
+        int start = 2;
+        int finish = 12;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 42;
         assertThat(result).isEqualTo(expected);
     }
 }
