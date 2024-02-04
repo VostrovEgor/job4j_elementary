f431b67fa5de2ca5f175cd755c16666188f8a272
vostrov <egor.voctrov.samara@gmail.com>
2024-02-04 22:54:32 +0400

    3. Перегрузить метод max для трех чисел

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -5,4 +5,12 @@ public class Max {
         int result = left > right ? left : right;
         return result;
     }
+
+    public static int max(int left, int right, int three) {
+        return max(three, max(left, right));
+    }
+
+    public static int max(int left, int right, int three, int four) {
+        return max(max(max(left, right), three), four);
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -12,4 +12,14 @@ class MaxTest {
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenMax2To2To3Then3() {
+        int left = 1;
+        int right = 2;
+        int three = 3;
+        int result = Max.max(left, right, three);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
 }
